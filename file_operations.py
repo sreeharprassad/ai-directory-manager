f6be025237437a7580db0521ce15724069645efc
sreeharprassad <sreeharprassad2005@gmail.com>
2026-03-16 19:28:54 +0530

    Implemented duplicate file detection using SHA-256 and duplicates folder

--- file_operations.py
+++ file_operations.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import hashlib
 from datetime import datetime
 from config import LOG_FILE
 
@@ -9,26 +10,52 @@ def log_action(message):
     # make sure logs folder exists
     os.makedirs("logs", exist_ok=True)
 
-    # open log file
     with open(LOG_FILE, "a") as file:
         file.write(f"{datetime.now()} - {message}\n")
 
     print(message)
 
 
+def get_file_hash(file_path):
+
+    hasher = hashlib.sha256()
+
+    with open(file_path, "rb") as f:
+        while chunk := f.read(4096):
+            hasher.update(chunk)
+
+    return hasher.hexdigest()
+
+
 def move_file(source, destination_folder):
 
-    # create folder if it doesn't exist
+    # create destination folder if needed
     os.makedirs(destination_folder, exist_ok=True)
 
-    # get filename
     filename = os.path.basename(source)
-
-    # destination path
     destination = os.path.join(destination_folder, filename)
 
-    # move file
+    # check if same filename exists
+    if os.path.exists(destination):
+
+        source_hash = get_file_hash(source)
+        destination_hash = get_file_hash(destination)
+
+        if source_hash == destination_hash:
+
+            # create duplicates folder
+            duplicate_folder = os.path.join("organized", "duplicates")
+            os.makedirs(duplicate_folder, exist_ok=True)
+
+            duplicate_path = os.path.join(duplicate_folder, filename)
+
+            shutil.move(source, duplicate_path)
+
+            log_action(f"Duplicate detected: {filename} moved to duplicates folder")
+
+            return
+
+    # move normally
     shutil.move(source, destination)
 
-    # log action
     log_action(f"{filename} moved to {destination_folder}")
